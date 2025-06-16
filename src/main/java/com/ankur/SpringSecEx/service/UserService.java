a0f0e5e8d0646164e799b53ca288891ad075cff4
Ankur Kumar <ankur.301203@gmail.com>
2025-06-16 23:44:29 +0530

    added register in UserService

--- src/main/java/com/ankur/SpringSecEx/service/UserService.java
+++ src/main/java/com/ankur/SpringSecEx/service/UserService.java
@@ -4,10 +4,13 @@ import com.ankur.SpringSecEx.model.Users;
 import com.ankur.SpringSecEx.repo.UserRepo;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class UserService {
@@ -17,11 +20,16 @@ public class UserService {
 
     private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
-    public Users register(Users user){
-        user.setPassword(encoder.encode(user.getPassword()));
-        return userRepo.save(user);
+    public ResponseEntity<?> register(Users user){
+        if(userRepo.existsByUsername(user.getUsername())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username already exists");
+        }
+        user.setPassword(encoder.encode((user.getPassword())));
+        userRepo.save(user);
+        return ResponseEntity.ok("User registered successfully");
 
     }
+
 
     public Users getUser(String id) {
         return userRepo.findById(Integer.parseInt(id)).orElse(null);
